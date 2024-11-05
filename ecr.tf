3353c707522fbb55fe2b1bd1d161406651513a64
f3lin <nguepi@jmetio.de>
2024-11-05 08:00:53 +0000

    Refactor ECR and IAM configurations to check for existing resources before creation

--- ecr.tf
+++ ecr.tf
@@ -1,70 +1,94 @@
-# data "aws_iam_policy_document" "push-pull-policy-document" {
-#   statement {
-#     sid    = "NewPolicy"
-#     effect = "Allow"
+# Check if ECR repositories already exist
+data "aws_ecr_repository" "existing_repositories" {
+  for_each = toset(var.repository_names)
+  name     = each.value
+}
 
-#     actions = [
-#       "ecr:GetDownloadUrlForLayer",
-#       "ecr:BatchGetImage",
-#       "ecr:BatchCheckLayerAvailability",
-#       "ecr:PutImage",
-#       "ecr:InitiateLayerUpload",
-#       "ecr:UploadLayerPart",
-#       "ecr:CompleteLayerUpload",
-#       "ecr:DescribeRepositories",
-#       "ecr:GetRepositoryPolicy",
-#       "ecr:ListImages",
-#       "ecr:DeleteRepository",
-#       "ecr:BatchDeleteImage",
-#       "ecr:SetRepositoryPolicy",
-#       "ecr:DeleteRepositoryPolicy",
-#     ]
+# Create IAM Policy Document for ECR Push/Pull
+data "aws_iam_policy_document" "push-pull-policy-document" {
+  statement {
+    sid    = "NewPolicy"
+    effect = "Allow"
 
-#     resources = [for repo in aws_ecr_repository.ecr-repositories : repo.arn]
-#   }
-# }
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeRepositories",
+      "ecr:GetRepositoryPolicy",
+      "ecr:ListImages",
+      "ecr:DeleteRepository",
+      "ecr:BatchDeleteImage",
+      "ecr:SetRepositoryPolicy",
+      "ecr:DeleteRepositoryPolicy",
+    ]
 
-# resource "aws_iam_policy" "push-pull-policy" {
-#   name        = "ECRPushPull"
-#   description = "Allow push pull image operations in ecr repositories"
-#   policy      = data.aws_iam_policy_document.push-pull-policy-document.json
-# }
+    resources = [for repo in aws_ecr_repository.ecr-repositories : repo.arn]
+  }
+}
 
-# resource "aws_iam_group_policy_attachment" "push-pull-policy-attachment" {
-#   group      = var.user_group
-#   policy_arn = aws_iam_policy.push-pull-policy.arn
-# }
+# IAM Policy: Create only if the policy doesn't already exist
+data "aws_iam_policy" "existing_policy" {
+  name = "ECRPushPull"
+}
 
-# resource "aws_ecr_repository" "ecr-repositories" {
-#   for_each = toset(var.repository_names)
+resource "aws_iam_policy" "push-pull-policy" {
+  count       = length(data.aws_iam_policy.existing_policy) == 0 ? 1 : 0
+  name        = "ECRPushPull"
+  description = "Allow push-pull image operations in ECR repositories"
+  policy      = data.aws_iam_policy_document.push-pull-policy-document.json
+}
 
-#   name = each.value
+# IAM Group Policy Attachment: Attach policy only if not already attached
+data "aws_iam_group_policy_attachment" "existing_attachment" {
+  for_each = toset(var.user_group)
+  group    = each.value
+  policy_arn = aws_iam_policy.push-pull-policy.arn
+}
 
-#   tags = local.tags
-# }
+resource "aws_iam_group_policy_attachment" "push-pull-policy-attachment" {
+  count      = length([for attachment in data.aws_iam_group_policy_attachment.existing_attachment : attachment if attachment.policy_arn == aws_iam_policy.push-pull-policy.arn]) == 0 ? 1 : 0
+  group      = var.user_group
+  policy_arn = aws_iam_policy.push-pull-policy.arn
+}
 
-# resource "aws_ecr_lifecycle_policy" "ecr-repositories-lifecycle-policy" {
-#   for_each = aws_ecr_repository.ecr-repositories
+# ECR Repository: Create repository only if it doesn't already exist
+resource "aws_ecr_repository" "ecr-repositories" {
+  for_each = toset(var.repository_names)
 
-#   repository = each.value.name
+  count = length([for repo in data.aws_ecr_repository.existing_repositories : repo if repo.name == each.value]) == 0 ? 1 : 0
 
-#   policy = <<EOF
-# {
-#     "rules": [
-#         {
-#             "rulePriority": 1,
-#             "description": "Keep last 30 images",
-#             "selection": {
-#                 "tagStatus": "tagged",
-#                 "tagPrefixList": ["v"],
-#                 "countType": "imageCountMoreThan",
-#                 "countNumber": 30
-#             },
-#             "action": {
-#                 "type": "expire"
-#             }
-#         }
-#     ]
-# }
-# EOF
-# }
+  name = each.value
+  tags = local.tags
+}
+
+# ECR Lifecycle Policy: Apply lifecycle policy for each repository
+resource "aws_ecr_lifecycle_policy" "ecr-repositories-lifecycle-policy" {
+  for_each = aws_ecr_repository.ecr-repositories
+
+  repository = each.value.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 30 images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 30
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
